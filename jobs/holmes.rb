8a026c22fc5e8d5e9e23b2fb3e7b56aeb08317cc
Pablo Santiago Blum de Aguiar <scorphus@gmail.com>
2014-07-21 20:21:26 -0300

    Display time in hours

--- jobs/holmes.rb
+++ jobs/holmes.rb
@@ -6,13 +6,13 @@ redis = Redis.new(:host => '10.11.165.8', :port => 49153)
 
 SCHEDULER.every '2s' do
 
-    domains_details_ttl = redis.ttl('domains_details')
-    violation_count_for_domains_ttl = redis.ttl('violation_count_for_domains')
-    violation_count_by_category_for_domains_ttl = redis.ttl('violation_count_by_category_for_domains')
-    top_violations_in_category_for_domains_ttl = redis.ttl('top_violations_in_category_for_domains')
-    blacklist_domain_count_ttl = redis.ttl('blacklist_domain_count')
-    most_common_violations_ttl = redis.ttl('most_common_violations')
-    failed_responses_count_ttl = redis.ttl('failed_responses_count')
+    domains_details_ttl = '%02.2f' % (redis.ttl('domains_details') / 3600.0)
+    violation_count_for_domains_ttl = '%02.2f' % (redis.ttl('violation_count_for_domains') / 3600.0)
+    violation_count_by_category_for_domains_ttl = '%02.2f' % (redis.ttl('violation_count_by_category_for_domains') / 3600.0)
+    top_violations_in_category_for_domains_ttl = '%02.2f' % (redis.ttl('top_violations_in_category_for_domains') / 3600.0)
+    blacklist_domain_count_ttl = '%02.2f' % (redis.ttl('blacklist_domain_count') / 3600.0)
+    most_common_violations_ttl = '%02.2f' % (redis.ttl('most_common_violations') / 3600.0)
+    failed_responses_count_ttl = '%02.2f' % (redis.ttl('failed_responses_count') / 3600.0)
 
     materials['domains_details_ttl'] = {label: 'domains_details_ttl', value: domains_details_ttl}
     materials['violation_count_for_domains_ttl'] = {label: 'violation_count_for_domains_ttl', value: violation_count_for_domains_ttl}
@@ -31,7 +31,7 @@ SCHEDULER.every '2s' do
     for key in all_keys
         if key =~ /.+-_LOCK_/
             key_ttl = redis.ttl(key)
-            locks[key] = {label: key.gsub(/(.+)-_LOCK_/, '\1'), value:key_ttl}
+            locks[key] = {label: key.gsub(/(.+)-_LOCK_/, '\1'), value: '%02.2f' % (key_ttl / 3600.0)}
         end
     end
 
