c1b407efbb2cac55d3d143b176bafe5772dfc822
Pablo Santiago Blum de Aguiar <scorphus@gmail.com>
2014-07-22 09:57:16 -0300

    Create a new dashboard for TV

--- jobs/holmes.rb
+++ jobs/holmes.rb
@@ -6,32 +6,30 @@ redis = Redis.new(:host => '10.11.165.8', :port => 49153)
 
 SCHEDULER.every '2s' do
 
-    domains_details_ttl = '%02.2f' % (redis.ttl('domains_details') / 3600.0)
-    violation_count_for_domains_ttl = '%02.2f' % (redis.ttl('violation_count_for_domains') / 3600.0)
-    violation_count_by_category_for_domains_ttl = '%02.2f' % (redis.ttl('violation_count_by_category_for_domains') / 3600.0)
-    top_violations_in_category_for_domains_ttl = '%02.2f' % (redis.ttl('top_violations_in_category_for_domains') / 3600.0)
-    blacklist_domain_count_ttl = '%02.2f' % (redis.ttl('blacklist_domain_count') / 3600.0)
-    most_common_violations_ttl = '%02.2f' % (redis.ttl('most_common_violations') / 3600.0)
-    failed_responses_count_ttl = '%02.2f' % (redis.ttl('failed_responses_count') / 3600.0)
+    domains_details_ttl = redis.ttl('domains_details')
+    violation_count_for_domains_ttl = redis.ttl('violation_count_for_domains')
+    violation_count_by_category_for_domains_ttl = redis.ttl('violation_count_by_category_for_domains')
+    top_violations_in_category_for_domains_ttl = redis.ttl('top_violations_in_category_for_domains')
+    blacklist_domain_count_ttl = redis.ttl('blacklist_domain_count')
+    most_common_violations_ttl = redis.ttl('most_common_violations')
+    failed_responses_count_ttl = redis.ttl('failed_responses_count')
 
-    materials['domains_details_ttl'] = {label: 'domains_details_ttl', value: domains_details_ttl}
-    materials['violation_count_for_domains_ttl'] = {label: 'violation_count_for_domains_ttl', value: violation_count_for_domains_ttl}
-    materials['violation_count_by_category_for_domains_ttl'] = {label: 'violation_count_by_category_for_domains_ttl', value: violation_count_by_category_for_domains_ttl}
-    materials['top_violations_in_category_for_domains_ttl'] = {label: 'top_violations_in_category_for_domains_ttl', value: top_violations_in_category_for_domains_ttl}
-    materials['blacklist_domain_count_ttl'] = {label: 'blacklist_domain_count_ttl', value: blacklist_domain_count_ttl}
-    materials['most_common_violations_ttl'] = {label: 'most_common_violations_ttl', value: most_common_violations_ttl}
-    materials['failed_responses_count_ttl'] = {label: 'failed_responses_count_ttl', value: failed_responses_count_ttl}
-
-    send_event('materials', {items: materials.values})
+    send_event('domains_details_ttl', {value: domains_details_ttl})
+    send_event('violation_count_for_domains_ttl', {value: violation_count_for_domains_ttl})
+    send_event('violation_count_by_category_for_domains_ttl', {value: violation_count_by_category_for_domains_ttl})
+    send_event('top_violations_in_category_for_domains_ttl', {value: top_violations_in_category_for_domains_ttl})
+    send_event('blacklist_domain_count_ttl', {value: blacklist_domain_count_ttl})
+    send_event('most_common_violations_ttl', {value: most_common_violations_ttl})
+    send_event('failed_responses_count_ttl', {value: failed_responses_count_ttl})
 
     all_keys = redis.keys('*')
-    # locks = all_keys.find_all { |key| key =~ /.+-_LOCK_/ }
 
     locks = Hash.new({ value: 0 })
     for key in all_keys
         if key =~ /.+-_LOCK_/
             key_ttl = redis.ttl(key)
-            locks[key] = {label: key.gsub(/(.+)-_LOCK_/, '\1'), value: '%02.2f' % (key_ttl / 3600.0)}
+            label = (key.gsub(/(.+)-_LOCK_/, '\1').split(/_/).map {|l| l[0]}).join('').upcase()
+            locks[key] = {label: label, value: '%02.2f' % (key_ttl / 60.0)}
         end
     end
 
