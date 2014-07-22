838faf03d1f948e87680ee551e4002d63916ac1b
Pablo Santiago Blum de Aguiar <scorphus@gmail.com>
2014-07-22 15:57:22 -0300

    New name, new color, new value...

--- jobs/holmes-workers.rb
+++ jobs/holmes-workers.rb
@@ -3,20 +3,22 @@ require 'open-uri'
 
 grapher_url = 'http://datacenter.globoi.com/grapher.cgi?target=%2Fservers%2Flinux%2Friomp97lb03%2Fnet%2Feth2;ranges=d;view=Octets'
 
-bits_out = []
+bits_in_out = []
 x_index = 0
 
 SCHEDULER.every '5m' do
     grapher = Nokogiri::HTML(open(grapher_url))
     tds = grapher.xpath('.//td[contains(.,"Average bits out")]')
-    bit_out = tds[1].content.match(/cur: ([0-9]*)/i)[1].to_i
 
-    if bits_out.count > 24 * 12 # keep last 24 hours
-        bits_out.shift
+    bits_in = tds[0].content.match(/cur: ([0-9]+)/i)[1].to_i
+    bits_out = tds[1].content.match(/cur: ([0-9]+)/i)[1].to_i
+
+    if bits_in_out.count > 24 * 12 # keep last 24 hours
+        bits_in_out.shift
     end
 
     x_index += 1
-    bits_out << {x: x_index, y: bit_out}
+    bits_in_out << {x: x_index, y: bits_in + bits_out}
 
-    send_event('bits_out', points: bits_out)
+    send_event('bits_in_out', points: bits_in_out)
 end
