c5890e6a7e78770aef93c5494f09798a6c1e423c
Pablo Santiago Blum de Aguiar <scorphus@gmail.com>
2014-07-24 19:40:00 -0300

    Improve code and read ttl from expired, if so

--- jobs/holmes-material.rb
+++ jobs/holmes-material.rb
@@ -4,21 +4,23 @@ redis = Redis.new(:host => '10.11.165.8', :port => 49153)
 
 SCHEDULER.every '5s' do
 
-    domains_details_ttl = redis.ttl('domains_details')
-    violation_count_for_domains_ttl = redis.ttl('violation_count_for_domains')
-    violation_count_by_category_for_domains_ttl = redis.ttl('violation_count_by_category_for_domains')
-    top_violations_in_category_for_domains_ttl = redis.ttl('top_violations_in_category_for_domains')
-    blacklist_domain_count_ttl = redis.ttl('blacklist_domain_count')
-    most_common_violations_ttl = redis.ttl('most_common_violations')
-    failed_responses_count_ttl = redis.ttl('failed_responses_count')
+    ttl = {
+        'domains_details' => 0,
+        'violation_count_for_domains' => 0,
+        'violation_count_by_category_for_domains' => 0,
+        'top_violations_in_category_for_domains' => 0,
+        'blacklist_domain_count' => 0,
+        'most_common_violations' => 0,
+        'failed_responses_count' => 0
+    }
 
-    send_event('domains_details_ttl', {value: domains_details_ttl})
-    send_event('violation_count_for_domains_ttl', {value: violation_count_for_domains_ttl})
-    send_event('violation_count_by_category_for_domains_ttl', {value: violation_count_by_category_for_domains_ttl})
-    send_event('top_violations_in_category_for_domains_ttl', {value: top_violations_in_category_for_domains_ttl})
-    send_event('blacklist_domain_count_ttl', {value: blacklist_domain_count_ttl})
-    send_event('most_common_violations_ttl', {value: most_common_violations_ttl})
-    send_event('failed_responses_count_ttl', {value: failed_responses_count_ttl})
+    ttl.keys().each do |k|
+        ttl[k] = redis.ttl(k)
+        if ttl[k] < 0
+            ttl[k] = redis.ttl("_expired_#{k}")
+        end
+        send_event(k, {value: ttl[k]})
+    end
 
     all_keys = redis.keys('*')
 
